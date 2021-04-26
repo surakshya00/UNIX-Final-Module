8376e1f4e48e688f0aaa318583e186b1f1206083
surakshyaa <surakshya.aryal@bison.howard.edu>
2021-04-25 23:28:53 -0500

    implemented simple ceaser cipher encryption

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,27 @@
 #mycipher
+import sys
+key=int(sys.argv[1])
+text_array=sys.stdin.readlines()
+text="".join(ele for ele in text_array)
+
+def ceasar_cipher(key,text):
+  count=0
+  new_text=""
+  words=0
+  for ele in text:
+    if ele.isalpha():
+      a=ord(ele.upper())+(key%26)
+      if a>ord("Z"):
+        a-=26
+      count+=1
+      if (count)%5==0:
+        new_text+=chr(a)+" "
+        count=0
+        words+=1
+        if words%10==0:
+          new_text+="\n"
+      else:
+        new_text+=chr(a)
+  return new_text
+
+print(ceasar_cipher(key,text))
